294ed7970e1b4a70046d5805398252db1ecb3753
Carlos <carlosgarcia_97gr@hotmail.com>
2020-06-26 23:45:13 -0500

    no recuerdo

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.12
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
-	github.com/go-ozzo/ozzo-validation/v4 v4.2.1 // indirect
+	github.com/go-ozzo/ozzo-validation/v4 v4.2.1
 	github.com/gorilla/mux v1.7.4
 	github.com/rs/cors v1.7.0
 	go.mongodb.org/mongo-driver v1.3.2
--- models/work.go
+++ models/work.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -16,4 +17,16 @@ type Work struct {
 	UserID      string             `bson:"user_id" json:"user_id,omitempty"`
 	AddressID   string             `bson:"address_id" json:"address_id,omitempty"`
 	CategoryID  string             `bson:"category_id" json:"category_id,omitempty"`
+}
+
+func (w Work) Validate() error {
+	return validation.ValidateStruct(&w,
+		validation.Field(&w.Name, validation.Required,validation.Length(5, 50)),
+		validation.Field(&w.Description, validation.Required,validation.Length(20, 50)),
+		validation.Field(&w.Price, validation.Required, validation.Min(20.00),validation.Max(400.00)),
+		validation.Field(&w.CreateAt,validation.Required),
+		validation.Field(&w.UserID, validation.Required),
+		validation.Field(&w.AddressID, validation.Required),
+		validation.Field(&w.CategoryID, validation.Required),
+	)
 }
